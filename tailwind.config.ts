c415e1e31415ec6d23b00ff531780e6b147f6da7
MicaCblls <micaelaceballos411@gmail.com>
2024-04-22 20:33:21 -0300

    first commit: add tailwind config, fonts and css reset

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,21 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      screens: {
+        xs: "320px",
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+        "2xl": "1680px",
+        "3xl": "1860px",
+      },
+      colors: {
+        lightGreen: "#ddf247",
+      },
+      fontFamily: {
+        sans: ["var(--inter-font)"],
+        lato: ["var(--lato-font)"],
       },
     },
   },
